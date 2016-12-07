10fc4da6ad71299c88c7cbab2e3d91a4af0cc1d0
Guillaume Denis <denisgme@gmail.com>
2016-12-07 16:45:32 +0100

    ajoute les behaviors

---
+++ src/main/java/fr/miage/supermarche/behavior/FournisseurBehavior.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.miage.supermarche.behavior;
+
+import jade.core.behaviours.CyclicBehaviour;
+
+/**
+ *
+ * @author Guillaume DENIS
+ */
+public class FournisseurBehavior extends CyclicBehaviour{
+
+    @Override
+    public void action() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
