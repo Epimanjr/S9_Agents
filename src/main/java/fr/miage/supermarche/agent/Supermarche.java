42e7b9d41e7c62bfae44f977817173fb886c2850
Guillaume Denis <denisgme@gmail.com>
2016-12-07 16:36:11 +0100

    on enregistre l'agent à la douane des jaunes

--- src/main/java/fr/miage/supermarche/agent/Supermarche.java
+++ src/main/java/fr/miage/supermarche/agent/Supermarche.java
@@ -1,7 +1,10 @@
 package fr.miage.supermarche.agent;
 
 import jade.core.Agent;
-
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
 /**
  *
  * @author Antoine NOSAL
@@ -13,6 +16,19 @@ public class Supermarche extends Agent {
 
     protected void setup() {
         System.out.println("Je suis une super supermarché dont le nom est : " + this.getLocalName());
+
+        //enregistre l'agent dans les pages jaunes
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("supermarche");
+        sd.setName(this.getLocalName());
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
     }
-    
+
 }
