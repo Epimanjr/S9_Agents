1e98aaa4680caee4a9a82bd3d48ee9fddbebf5d4
Guillaume Denis <denisgme@gmail.com>
2016-12-07 23:07:54 +0100

    ajoute le squelette pour la demande de distance et la réponse

--- src/main/java/fr/miage/supermarche/behavior/ClientBehavior.java
+++ src/main/java/fr/miage/supermarche/behavior/ClientBehavior.java
@@ -6,26 +6,24 @@
 package fr.miage.supermarche.behavior;
 
 import fr.miage.agents.api.message.Message;
-import fr.miage.agents.api.message.auth.DemanderSession;
-import fr.miage.agents.api.message.auth.ResultatDemandeSession;
 import fr.miage.agents.api.message.recherche.Rechercher;
 import fr.miage.agents.api.message.recherche.ResultatRecherche;
 import fr.miage.agents.api.message.relationclientsupermarche.Achat;
+import fr.miage.agents.api.message.relationclientsupermarche.ResultatDistance;
 import fr.miage.agents.api.model.Produit;
-import jade.core.AID;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.UnreadableException;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Guillaume DENIS
  */
-public class ClientBehavior extends CyclicBehaviour{
+public class ClientBehavior extends CyclicBehaviour {
 
     @Override
     public void action() {
@@ -33,47 +31,59 @@ public class ClientBehavior extends CyclicBehaviour{
 
         // Notre liste de produits
         List<Produit> produits;
-        
+
         // On gère le message reçu par la plateforme
-        if(aclMsg != null){
+        if (aclMsg != null) {
             try {
                 // Récupèration du Message de l'API
-                Message msg = (Message)aclMsg.getContentObject();
+                Message recu = (Message) aclMsg.getContentObject();
 
-                switch(msg.type){
+                switch (recu.type) {
                     // Le client désire rechercher un Produit
                     case Recherche:
-                        Rechercher r = (Rechercher)msg;
-                        
+                        Rechercher r = (Rechercher) recu;
+
                         //TODO récupérer le produit depuis nos données
                         produits = null;
-                        
+
                         // on met notre liste dans un message
                         ResultatRecherche rr = new ResultatRecherche();
                         rr.produitList = produits;
                         rr.Session = r.session;
                         break;
-                        
+
                     // On reçoit une demande pour acheter des produits
                     case InitierAchat:
-                        Achat a = (Achat)msg;
-                        
+                        Achat a = (Achat) recu;
+
                         // traiter la liste de courses
-                        
                         // renvoyer un résultat achat
                         break;
                     // Demande de la distance
-                    case Distance:
+                    case DemandeDistance:
+                        // On récupère la distance \m/
+                        double distance = 66.6;
+                        int delai = 3;
+                        ResultatDistance rd = new ResultatDistance();
+                        rd.distance = distance;
+                        rd.delai = delai;
+                        
+                        // Envoit du résultat
+                        ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+                        msg.setContentObject(rd);
+                        this.getAgent().send(msg);
+                        
                         break;
                     default:
                         break;
                 }
 
             } catch (UnreadableException e) {
-                e.printStackTrace();
-            } 
+                Logger.getLogger(ClientBehavior.class.getName()).log(Level.SEVERE, null, e);
+            } catch (IOException ex) {
+                Logger.getLogger(ClientBehavior.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
-    
-    
+
 }
