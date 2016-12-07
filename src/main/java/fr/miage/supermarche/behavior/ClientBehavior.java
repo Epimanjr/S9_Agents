c26f57ef5c949665a895a432f1f9d6dc917faa6a
Guillaume Denis <denisgme@gmail.com>
2016-12-07 18:59:58 +0100

    ajoute le squelette pour le comportement client

--- src/main/java/fr/miage/supermarche/behavior/ClientBehavior.java
+++ src/main/java/fr/miage/supermarche/behavior/ClientBehavior.java
@@ -6,9 +6,20 @@
 package fr.miage.supermarche.behavior;
 
 import fr.miage.agents.api.message.Message;
+import fr.miage.agents.api.message.auth.DemanderSession;
+import fr.miage.agents.api.message.auth.ResultatDemandeSession;
+import fr.miage.agents.api.message.recherche.Rechercher;
+import fr.miage.agents.api.message.recherche.ResultatRecherche;
+import fr.miage.agents.api.message.relationclientsupermarche.Achat;
+import fr.miage.agents.api.model.Produit;
+import jade.core.AID;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.UnreadableException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  *
@@ -20,6 +31,9 @@ public class ClientBehavior extends CyclicBehaviour{
     public void action() {
         ACLMessage aclMsg = this.getAgent().receive();
 
+        // Notre liste de produits
+        List<Produit> produits;
+        
         // On gère le message reçu par la plateforme
         if(aclMsg != null){
             try {
@@ -27,8 +41,29 @@ public class ClientBehavior extends CyclicBehaviour{
                 Message msg = (Message)aclMsg.getContentObject();
 
                 switch(msg.type){
+                    // Le client désire rechercher un Produit
                     case Recherche:
-                        //TODO gérer la recherche par rapport à des critères
+                        Rechercher r = (Rechercher)msg;
+                        
+                        //TODO récupérer le produit depuis nos données
+                        produits = null;
+                        
+                        // on met notre liste dans un message
+                        ResultatRecherche rr = new ResultatRecherche();
+                        rr.produitList = produits;
+                        rr.Session = r.session;
+                        break;
+                        
+                    // On reçoit une demande pour acheter des produits
+                    case InitierAchat:
+                        Achat a = (Achat)msg;
+                        
+                        // traiter la liste de courses
+                        
+                        // renvoyer un résultat achat
+                        break;
+                    // Demande de la distance
+                    case Distance:
                         break;
                     default:
                         break;
@@ -36,8 +71,7 @@ public class ClientBehavior extends CyclicBehaviour{
 
             } catch (UnreadableException e) {
                 e.printStackTrace();
-            }
-
+            } 
         }
     }
     
