74d92d4de6263025abd21c201b0ba94ab14f2e06
Antoine Nosal <antoine.nosal@gmail.com>
2016-12-10 02:42:53 +0100

    complète la stratégie de tarification

--- src/main/java/fr/miage/supermarche/util/strategy/StockSimpleStrategy.java
+++ src/main/java/fr/miage/supermarche/util/strategy/StockSimpleStrategy.java
@@ -58,7 +58,7 @@ public class StockSimpleStrategy implements StockStrategy {
                 Produit produit = Produit.getByReference(qteMinProduit.getKey());
                 // On utilise la stratégie (définie en début de classe)
                 if (produit.getStock() < (qteMinProduit.getValue() * this.seuil)) {
-                    qteManquante = qteMinProduit.getValue() - produit.getStock();
+                    qteManquante = (int) ((qteMinProduit.getValue()*1.20) - produit.getStock());
                     s.aCommander.put(produit.getId(), qteManquante);
                 }
             }
--- src/main/java/fr/miage/supermarche/util/strategy/TarificationSimpleStrategy.java
+++ src/main/java/fr/miage/supermarche/util/strategy/TarificationSimpleStrategy.java
@@ -1,10 +1,16 @@
 package fr.miage.supermarche.util.strategy;
 
+import fr.miage.supermarche.persist.Produit;
 import fr.miage.supermarche.util.Tarification;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Définit une stratégie simple de tarification
- * Basiquement, on pourrait définir un prix de vente HT par produit
+ * Basiquement, on définit une marge par catégorie
  * 
  * @author Antoine NOSAL
  * @author Maxime BLAISE
@@ -13,9 +19,80 @@ import fr.miage.supermarche.util.Tarification;
  */
 public class TarificationSimpleStrategy implements TarificationStrategy {
 
+    /**
+     * Estimation des marges fournisseurs sur prix producteur
+     * par catégorie
+     * 
+     * Exemple :
+     * (1, 1.30f) => On estime que les fournisseurs multiplie par 1.30
+     * le prix producteur des produits de la catégorie 1 pour faire leur prix de vente
+     * 
+     * @param p 
+     */
+    private Map<Integer, Float> mgFourniSurProd;
+    
+    /**
+     * Marges définies sur prix fournisseur
+     * par catégorie
+     * 
+     * Exemple :
+     * (2, 1.50f) => On multiplie par 1.50 les prix d'achat pour
+     * obtenir nos prix du vente sur les produits de la catégorie 2
+     * 
+     */
+    private Map<Integer, Float> mgSupermarcheSurFourni;
+    
+    private void initMgFourniSurProd() {
+        this.mgFourniSurProd.put(1, 1.30f);
+        this.mgFourniSurProd.put(2, 1.30f);
+        this.mgFourniSurProd.put(3, 1.30f);
+        this.mgFourniSurProd.put(4, 1.30f);
+        this.mgFourniSurProd.put(5, 1.30f);
+        this.mgFourniSurProd.put(6, 1.30f);
+    }
+    
+    private void initMgSupermarcheFourni() {
+        this.mgSupermarcheSurFourni.put(1, 1.50f);
+        this.mgSupermarcheSurFourni.put(2, 1.50f);
+        this.mgSupermarcheSurFourni.put(3, 1.50f);
+        this.mgSupermarcheSurFourni.put(4, 1.50f);
+        this.mgSupermarcheSurFourni.put(5, 1.50f);
+        this.mgSupermarcheSurFourni.put(6, 1.50f); 
+    }
+    
     @Override
     public void update(Tarification p) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Float prixAchat;
+        Float prixProducteur;
+        Float prixVente;
+        Integer idCategorie;
+        try {
+            List<Produit> produits = Produit.getAllProduit();
+            for(Produit produit : produits) {
+                //idCategorie = Produit.getIdCategorie();
+                idCategorie = 1;
+                
+                //prixAchat = produit.getPrixAchat;
+                prixAchat = 1.0f;
+                
+                if(prixAchat != (-1.0f)) {
+                    // Si on a déjà une fois acheté ce produit
+                    prixVente = prixAchat * this.mgSupermarcheSurFourni.get(idCategorie);
+                } else {
+                    //prixProducteur = produit.getPrixProducteur();
+                    prixProducteur = 0.30f;
+                    
+                    // On utilise une estimation de la marge producteur
+                    prixVente = prixAchat * this.mgFourniSurProd.get(idCategorie) * this.mgSupermarcheSurFourni.get(idCategorie);
+                }
+                
+                produit.setPrix(prixVente);
+                //produit.update();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(TarificationSimpleStrategy.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
 
 }
