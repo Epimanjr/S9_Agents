302368b37526c2dacc2db6e2bf307f94740c07ca
Antoine Nosal <antoine.nosal@gmail.com>
2016-12-10 01:03:20 +0100

    corrige des commentaires et commence la stratégie d'achat

--- src/main/java/fr/miage/supermarche/util/strategy/AchatFournisseurSimpleStrategy.java
+++ src/main/java/fr/miage/supermarche/util/strategy/AchatFournisseurSimpleStrategy.java
@@ -1,23 +1,48 @@
 package fr.miage.supermarche.util.strategy;
 
+import fr.miage.supermarche.persist.Produit;
 import fr.miage.supermarche.util.AchatFournisseur;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- * Définit une stratégie d'achat simple
- * basée sur le produit, son prix, la quantité souhaitée
- * et la quantité disponible en stock
+ * Définit une stratégie d'achat simple basée sur le produit, son prix, la
+ * quantité souhaitée et la quantité disponible en stock
+ *
  * @author Antoine NOSAL
  * @author Maxime BLAISE
  * @author Guillaume DÉNISSE
  * @author Geoffrey GAILLARD
  */
 public class AchatFournisseurSimpleStrategy implements AchatFournisseurStrategy {
-    
+
+    /**
+     * En fonction du produit (af.getIdProduit) et du prix (af.getPrix), de la
+     * quantité souhaitée (..) et de la quantité disponible (..) chez le
+     * fournisseur la stratégie nous dit si on achète ou non
+     *
+     * @param af Achat proposé à valider/refuser
+     * @return boolean true si on peut acheter
+     */
     @Override
     public boolean approuver(AchatFournisseur af) {
-        // En fonction du produit (af.getIdProduit) et du prix (af.getPrix),
-        // de la quantité souhaitée et de la quantité disponible chez le fournisseur
-        // la stratégie nous dit si on achète ou non
+        try {
+            // TODO : remplacer par la ligne suivante dès que la méthode sera dispo
+            //Produit produit = Produit.getById(af.getIdProduit());
+            Produit produit = Produit.getByReference("todo");
+            Float prixPropose = af.getPrixProposeParFournisseur();
+            Integer qteSouhaitee = af.getQteSouhaitee();
+            Integer qteDisponible = af.getQteDisponibleChezFournisseur();
+            
+            // TODO : approuver ou non la proposition d'achat
+            
+            // TODO : Il faut aussi ici déterminer le prochain
+            // prix que l'on va proposer pour négocier
+            af.setPrixANegocier(0);
+        } catch (SQLException ex) {
+            Logger.getLogger(AchatFournisseurSimpleStrategy.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return false;
     }
 
--- src/main/java/fr/miage/supermarche/util/strategy/PeriodeSimpleStrategy.java
+++ src/main/java/fr/miage/supermarche/util/strategy/PeriodeSimpleStrategy.java
@@ -12,21 +12,26 @@ import fr.miage.supermarche.util.PeriodeType;
  */
 public class PeriodeSimpleStrategy implements PeriodeStrategy {
 
+    /**
+     * La période p en paramètre est la période actuelle.
+     * La période retournée est la nouvelle période (p+1)
+     * L'idée, c'est de savoir si on est :
+     *   - en période standard (jan. -> oct.)
+     *          (impossible de vendre à perte)
+     *   - en période de soldes des fêtes (nov. -> déc.)
+     *          (possible de faire des promotions en réduisant les prix jusqu’à 30%
+     *           à condition qu’il s’agisse de lots de produits différents)
+     *   - en période de soldes flottantes (quand on veut, 10 jours max deux fois par an)
+     *          (prévenir tous les agents au moins 2 semaines avant, 
+     *           possible de faire des réductions sans aller en-deçà du prix coûtant)
+     * NB: Si la nouvelle période est la même que celle en paramètre,
+     * Alors on retourne le paramètre p.
+     * 
+     * @param p         Période actuelle
+     * @return Periode  Nouvelle période
+     */
     @Override
     public Periode define(Periode p) {
-        /**
-         * La période p en paramètre est la période actuelle.
-         * La période retournée est la nouvelle période (p+1)
-         * L'idée, c'est de savoir si on est :
-         *   - en période standard (jan. -> oct.)
-         *          (impossible de vendre à perte)
-         *   - en période de soldes des fêtes (nov. -> déc.)
-         *          (possible de faire des promotions en réduisant les prix jusqu’à 30%
-         *           à condition qu’il s’agisse de lots de produits différents)
-         *   - en période de soldes flottantes (quand on veut, 10 jours max deux fois par an)
-         *          (prévenir tous les agents au moins 2 semaines avant, 
-         *           possible de faire des réductions sans aller en-deçà du prix coûtant)
-         */
         Periode nouvellePeriode = new Periode(null, null, PeriodeType.STANDARD);
         return nouvellePeriode;
     }
