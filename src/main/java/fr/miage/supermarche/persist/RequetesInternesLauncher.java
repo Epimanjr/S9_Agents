d9f3b2eed8b5b4a93aeec5c6bf8c267308ed829c
Guillaume Denis <denisgme@gmail.com>
2016-12-11 09:37:31 +0100

    change les types des ids produits

--- src/main/java/fr/miage/supermarche/persist/RequetesInternesLauncher.java
+++ src/main/java/fr/miage/supermarche/persist/RequetesInternesLauncher.java
@@ -8,15 +8,15 @@ import java.util.Iterator;
 public class RequetesInternesLauncher {
     
     public static void testRetirerProduits() {
-        HashMap<Integer, Integer> mapProduits = new HashMap<>();
-        mapProduits.put(770584450, 2);
-        mapProduits.put(771269767, 2);
+        HashMap<Long, Integer> mapProduits = new HashMap<>();
+        mapProduits.put(new Long(770584450), 2);
+        mapProduits.put(new Long(771269767), 2);
         
-        HashMap<Integer, Boolean> mapResults = RequetesInternes.retirerProduits(mapProduits);
-        Iterator<Integer> itResults = mapResults.keySet().iterator();
+        HashMap<Long, Integer> mapResults = RequetesInternes.retirerProduits(mapProduits);
+        Iterator<Long> itResults = mapResults.keySet().iterator();
         while (itResults.hasNext()) {
-            Integer idProduit = itResults.next();
-            Boolean result = mapResults.get(idProduit);
+            Long idProduit = itResults.next();
+            Integer result = mapResults.get(idProduit);
             
             System.out.println(idProduit + " => " + result);
         }
